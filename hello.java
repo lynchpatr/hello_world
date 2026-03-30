961209a5c6c68e6a7cd7e94356223eba42983297
lynchpatr <lynchpatr@my.easternct.edu>
2026-03-30 16:46:05 -0400

    modified:   .vscode/launch.json modified:   hello.java

--- hello.java
+++ hello.java
@@ -1,17 +1,13 @@
-// a simple program to practice common Github commands 
-
+  
 public class hello {
 
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
+       String firstName = "Patrick";
 
-       // delete this line
-       System.out.println("Modify this code");
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
-
+       System.out.println("Hello, I am " + firstName);
     }
 }
-
